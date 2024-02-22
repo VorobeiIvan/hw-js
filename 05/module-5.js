b970098e877fe8cb7fa2d8d40839cedcfe0f3ad7
Ivan <0675361905vi@gmail.com>
2024-02-22 18:01:16 +0200

    add answer1-6

--- 05/module-5.js
+++ 05/module-5.js
@@ -2,6 +2,7 @@
 // _______________________________________________________________________________
 // _______________________________________________________________________________
 
+// #region task1
 console.log('%cTask1', 'color: green; font-weight: bold;');
 // Виконай рефакторинг методів об'єкта pizzaPalace, розставивши відсутні this в місцях звернення до властивостей і методів об'єкта.
 
@@ -11,24 +12,29 @@ console.log('%cTask1', 'color: green; font-weight: bold;');
 // Виклик pizzaPalace.order("Four meats") повертає рядок "Order accepted, preparing «Four meats» pizza"
 // Виклик pizzaPalace.order("Big Mike") повертає рядок "Sorry, there is no pizza named «Big Mike»"
 // Виклик pizzaPalace.order("Viennese") повертає рядок "Sorry, there is no pizza named «Viennese»"
-// const pizzaPalace = {
-//     pizzas: ["Supercheese", "Smoked", "Four meats"],
-//     // Change code below this line
-//     checkPizza(pizzaName) {
-//       return pizzas.includes(pizzaName);
-//     },
-//     order(pizzaName) {
-//       const isPizzaAvailable = checkPizza(pizzaName);
+const pizzaPalace = {
+  pizzas: ['Supercheese', 'Smoked', 'Four meats'],
 
-//       if (!isPizzaAvailable) {
-//         return `Sorry, there is no pizza named «${pizzaName}»`;
-//       }
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
 
-//       return `Order accepted, preparing «${pizzaName}» pizza`;
-//     },
-//     // Change code above this line
-//   };
+    if (!isPizzaAvailable) {
+      return `Sorry, there is no pizza named «${pizzaName}»`;
+    }
 
+    return `Order accepted, preparing «${pizzaName}» pizza`;
+  },
+};
+
+console.log(pizzaPalace.order('Smoked')); // повертає рядок "Order accepted, preparing «Smoked» pizza"
+console.log(pizzaPalace.order('Four meats')); // повертає рядок "Order accepted, preparing «Four meats» pizza"
+console.log(pizzaPalace.order('Big Mike')); // повертає рядок "Sorry, there is no pizza named «Big Mike»"
+console.log(pizzaPalace.order('Viennese')); // повертає рядок "Sorry, there is no pizza named «Viennese»"
+// #endregion
+// #region task2
 console.log('%cTask2', 'color: green; font-weight: bold;');
 
 // Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі. Виконай рефакторинг методів об'єкта customer, розставивши відсутні this під час звернення до властивостей об'єкта.
@@ -47,36 +53,38 @@ console.log('%cTask2', 'color: green; font-weight: bold;');
 // Метод setDiscount об'єкта customer використовує this
 // Метод getOrders об'єкта customer використовує this
 // Метод addOrder об'єкта customer використовує this
-// const customer = {
-//   username: 'Mango',
-//   balance: 24000,
-//   discount: 0.1,
-//   orders: ['Burger', 'Pizza', 'Salad'],
-//   // Change code below this line
-//   getBalance() {
-//     return balance;
-//   },
-//   getDiscount() {
-//     return discount;
-//   },
-//   setDiscount(value) {
-//     discount = value;
-//   },
-//   getOrders() {
-//     return orders;
-//   },
-//   addOrder(cost, order) {
-//     balance -= cost - cost * discount;
-//     orders.push(order);
-//   },
-//   // Change code above this line
-// };
+const customer = {
+  username: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['Burger', 'Pizza', 'Salad'],
 
-// customer.setDiscount(0.15);
-// console.log(customer.getDiscount()); // 0.15
-// customer.addOrder(5000, 'Steak');
-// console.log(customer.getBalance()); // 19750
-// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, 'Steak');
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+// #endregion
+// #region task3
 
 console.log('%cTask3', 'color: green; font-weight: bold;');
 // Тестувальники знайшли баги в коді сервісу зберігання історії замовлень їжі. Тобі необхідно виправити їх, правильно розставивши this в методах об'єкта historyService, щоб методи почали працювати правильно.
@@ -90,30 +98,37 @@ console.log('%cTask3', 'color: green; font-weight: bold;');
 // Метод getOrdersLog об'єкта historyService використовує this
 // Метод getEmails об'єкта historyService використовує this
 // Метод getOrdersByEmail об'єкта historyService використовує this
-// const historyService = {
-//     orders: [
-//       { email: "jacob@hotmail.com", dish: "Burrito" },
-//       { email: "solomon@topmail.net", dish: "Burger" },
-//       { email: "artemis@coldmail.net", dish: "Pizza" },
-//       { email: "solomon@topmail.net", dish: "Apple pie" },
-//       { email: "jacob@hotmail.com", dish: "Taco" },
-//     ],
-//     // Change code below this line
-//     getOrdersLog() {
-//       return orders
-//         .map(order => `email: ${order.email} dish: ${order.dish}`)
-//         .join(" - ");
-//     },
-//     getEmails() {
-//       const emails = orders.map(order => order.email);
-//       const uniqueEmails = new Set(emails);
-//       return [...uniqueEmails];
-//     },
-//     getOrdersByEmail(email) {
-//       return orders.filter(order => order.email === email);
-//     },
-//     // Change code above this line
-//   };
+const historyService = {
+  orders: [
+    { email: 'jacob@hotmail.com', dish: 'Burrito' },
+    { email: 'solomon@topmail.net', dish: 'Burger' },
+    { email: 'artemis@coldmail.net', dish: 'Pizza' },
+    { email: 'solomon@topmail.net', dish: 'Apple pie' },
+    { email: 'jacob@hotmail.com', dish: 'Taco' },
+  ],
+
+  getOrdersLog() {
+    return this.orders
+      .map(order => `email: ${order.email} dish: ${order.dish}`)
+      .join(' - ');
+  },
+  getEmails() {
+    const emails = this.orders.map(order => order.email);
+    const uniqueEmails = new Set(emails);
+    return [...uniqueEmails];
+  },
+  getOrdersByEmail(email) {
+    return this.orders.filter(order => order.email === email);
+  },
+};
+
+console.log(historyService.getOrdersLog()); //повертає рядок з переліком даних всіх замовлень з властивості orders
+console.log(historyService.getEmails()); //повертає масив всіх унікальних поштових адрес з властивості orders
+console.log(historyService.getOrdersByEmail('solomon@topmail.net')); // повертає [{ email: "solomon@topmail.net", dish: "Burger" }, { email: "solomon@topmail.net", dish: "Apple pie" }]
+console.log(historyService.getOrdersByEmail('artemis@coldmail.net')); //повертає [{ email: "artemis@coldmail.net", dish: "Pizza" }]
+
+// #endregion
+// #region task4
 
 console.log('%cTask4', 'color: green; font-weight: bold;');
 // Зміни код таким чином, щоб об'єкт parent став прототипом для об'єкта у змінній сhild.
@@ -134,20 +149,33 @@ console.log('%cTask4', 'color: green; font-weight: bold;');
 // Звернення до child.heritage повертає "Irish"
 // Виклик parent.isPrototypeOf(child) повертає true
 // Використовується метод Object.create()
-// const parent = {
-//     name: "Stacey",
-//     surname: "Moore",
-//     age: 54,
-//     heritage: "Irish",
-//   };
-//   // Change code below this line
+const parent = {
+  name: 'Stacey',
+  surname: 'Moore',
+  age: 54,
+  heritage: 'Irish',
+};
 
-//   const child = {};
+const child = Object.create(parent);
 
-//   // Change code above this line
-//   child.name = "Jason";
-//   child.age = 27;
+child.name = 'Jason';
+child.age = 27;
 
+console.log(parent.hasOwnProperty('surname')); // повертає true
+console.log(parent.hasOwnProperty('heritage')); // повертає true
+console.log(child.hasOwnProperty('name')); // повертає true
+console.log(child.name); // повертає "Jason"
+console.log(child.hasOwnProperty('age')); // повертає true
+console.log(child.age); // повертає 27
+console.log(child.hasOwnProperty('surname')); // повертає false
+console.log(child.surname); // повертає "Moore"
+console.log(child.hasOwnProperty('heritage')); // повертає false
+console.log(child.heritage); // повертає "Irish"
+console.log(parent.isPrototypeOf(child)); // повертає true
+console.log('child', child);
+
+// #endregion
+// #region task5
 console.log('%cTask5', 'color: green; font-weight: bold;');
 
 // Зміни код, побудувавши ланцюжок прототипів таким чином, щоб об'єкт ancestor був прототипом для parent, а той, своєю чергою, був прототипом для child.
@@ -173,30 +201,52 @@ console.log('%cTask5', 'color: green; font-weight: bold;');
 // Виклик child.hasOwnProperty("heritage") повертає false
 // Звернення до child.heritage повертає "Irish"
 // Використовується метод Object.create()
-// const ancestor = {
-//     name: "Paul",
-//     age: 83,
-//     surname: "Dawson",
-//     heritage: "Irish",
-//   };
-//   // Change code below this line
+const ancestor = {
+  name: 'Paul',
+  age: 83,
+  surname: 'Dawson',
+  heritage: 'Irish',
+};
 
-//   const parent = {};
-//   parent.name = "Stacey";
-//   parent.surname = "Moore";
-//   parent.age = 54;
+const parent1 = Object.create(ancestor);
 
-//   const child = {};
-//   child.name = "Jason";
-//   child.age = 27;
+parent1.name = 'Stacey';
+parent1.surname = 'Moore';
+parent1.age = 54;
 
-//   // Change code above this line
+const child1 = Object.create(parent1);
+child1.name = 'Jason';
+child1.age = 27;
+
+console.log(ancestor.isPrototypeOf(parent1)); // повертає true
+console.log(parent1.isPrototypeOf(child1)); // повертає true
+console.log(ancestor.hasOwnProperty('surname')); // повертає true
+console.log(ancestor.surname); // повертає "Dawson"
+console.log(parent1.hasOwnProperty('surname')); // повертає true
+console.log(parent1.surname); // повертає "Moore"
+console.log(child1.hasOwnProperty('surname')); // повертає false
+console.log(child1.surname); // повертає "Moore"
+console.log(ancestor.hasOwnProperty('heritage')); // повертає true
+console.log(ancestor.heritage); // повертає "Irish"
+console.log(parent1.hasOwnProperty('heritage')); // повертає false
+console.log(parent1.heritage); // повертає "Irish"
+console.log(child1.hasOwnProperty('heritage')); //повертає false
+console.log(child1.heritage); //повертає "Irish"
+
+// #endregion
+// #region task6
 console.log('%cTask6', 'color: green; font-weight: bold;');
 // Використовуючи ключове слово class, оголоси клас Car з порожнім тілом.
 
 // Оголошений клас Car
 // Результат виклику new Car() - це порожній об'єкт
 
+class Car {}
+
+console.log('new Car() ', new Car());
+
+// #endregion
+// #region task7
 console.log('%cTask7', 'color: green; font-weight: bold;');
 // Додай класу Car метод constructor, який приймає три параметри:
 
@@ -210,12 +260,18 @@ console.log('%cTask7', 'color: green; font-weight: bold;');
 // В результаті виклику new Car("Audi", "Q3", 36000) утвориться об'єкт { brand: "Audi", model: "Q3", price: 36000 }
 // В результаті виклику new Car("BMW", "X5", 58900) утвориться об'єкт { brand: "BMW", model: "X5", price: 58900 }
 // В результаті виклику new Car("Nissan","Murano", 31700) утвориться об'єкт { brand: "Nissan", model: "Murano", price: 31700 }
-// class Car {
-//     // Change code below this line
 
-//     // Change code above this line
-//   }
+class Car {
+}
+const constructor: Car {
+  brand, model, price
+}
+console.log(new Car("Audi", "Q3", 36000))//утвориться об'єкт { brand: "Audi", model: "Q3", price: 36000 }
+console.log(new Car("BMW", "X5", 58900))// утвориться об'єкт { brand: "BMW", model: "X5", price: 58900 }
+console.log(new Car("Nissan","Murano", 31700))// утвориться об'єкт { brand: "Nissan", model: "Murano", price: 31700 }
 
+// #endregion
+// #region task8
 console.log('%cTask8', 'color: green; font-weight: bold;');
 // Виконай рефакторинг класу Car таким чином, щоб він приймав один параметр - об'єкт з властивостями brand, model і price. Деструктуризуй об'єкт в сигнатурі (підписі) конструктора.
 
@@ -234,6 +290,9 @@ console.log('%cTask8', 'color: green; font-weight: bold;');
 //     }
 //     // Change code above this line
 //   }
+
+// #endregion
+// #region task9
 console.log('%cTask9', 'color: green; font-weight: bold;');
 // Додай класу Car два методи.
 
@@ -254,6 +313,8 @@ console.log('%cTask9', 'color: green; font-weight: bold;');
 //     // Change code above this line
 //   }
 
+// #endregion
+// #region task10
 console.log('%cTask10', 'color: green; font-weight: bold;');
 
 // Напиши клас Storage, який буде створювати об'єкти для управління складом товарів. Клас очікує тільки один аргумент - початковий масив товарів, який записується у властивість items об'єкта, що створюється.
@@ -285,6 +346,9 @@ console.log('%cTask10', 'color: green; font-weight: bold;');
 // console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 // storage.removeItem("Prolonger");
 // console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// #endregion
+// #region task11
 console.log('%cTask11', 'color: green; font-weight: bold;');
 
 // Напиши клас StringBuilder, який приймає один параметр initialValue - довільний рядок, який записується у властивість value об'єкта, що створюється.
@@ -323,6 +387,8 @@ console.log('%cTask11', 'color: green; font-weight: bold;');
 // builder.padBoth("=");
 // console.log(builder.getValue()); // "=^.^="
 
+// #endregion
+// #region task12
 console.log('%cTask12', 'color: green; font-weight: bold;');
 
 // Виконай рефакторинг класу Car таким чином, щоб властивість brand була приватною, і додай два методи для публічного інтерфейсу, для читання і зміни цієї властивості.
@@ -350,6 +416,8 @@ console.log('%cTask12', 'color: green; font-weight: bold;');
 //     // Change code above this line
 //   }
 
+// #endregion
+// #region task13
 console.log('%cTask13', 'color: green; font-weight: bold;');
 
 // Виконай рефакторинг класу Storage, зробивши властивість items приватною.
@@ -396,6 +464,8 @@ console.log('%cTask13', 'color: green; font-weight: bold;');
 //   storage.removeItem("Prolonger");
 //   console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
 
+// #endregion
+// #region task14
 console.log('%cTask14', 'color: green; font-weight: bold;');
 
 // Виконай рефакторинг класу StringBuilder, зробивши властивість value приватною.
@@ -460,6 +530,8 @@ console.log('%cTask14', 'color: green; font-weight: bold;');
 //   builder.padBoth("=");
 //   console.log(builder.getValue()); // "=^.^="
 
+// #endregion
+// #region task15
 console.log('%cTask15', 'color: green; font-weight: bold;');
 
 // Виконай рефакторинг класу Car. Зроби властивості model і price приватними, а також #brand. Стандартизуй публічний інтерфейс класу, замінивши вже оголошені методи на гетери та сетери brand, model і price, для взаємодії з приватними властивостями.
@@ -510,6 +582,9 @@ console.log('%cTask15', 'color: green; font-weight: bold;');
 //     }
 //     // Change code above this line
 //   }
+
+// #endregion
+// #region task16
 console.log('%cTask16', 'color: green; font-weight: bold;');
 
 // Виконай рефакторинг класу Car. Додай публічну статичну властивість MAX_PRICE зі значенням 50000 - максимально допустима ціна автомобіля.
@@ -550,6 +625,9 @@ console.log('%cTask16', 'color: green; font-weight: bold;');
 
 //   audi.price = 51000;
 //   console.log(audi.price); // 49000
+
+// #endregion
+// #region task17
 console.log('%cTask17', 'color: green; font-weight: bold;');
 
 // Додай класу Car публічний статичний метод checkPrice (price), що приймає ціну автомобіля. Метод повинен порівняти значення параметра price і приватної статичного властивості MAX_PRICE.
@@ -580,6 +658,8 @@ console.log('%cTask17', 'color: green; font-weight: bold;');
 //   console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
 //   console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
 
+// #endregion
+// #region task18
 console.log('%cTask18', 'color: green; font-weight: bold;');
 
 // У застосунку потрібен адміністратор з можливістю додавати пошти користувачів у чорний список.
@@ -606,6 +686,8 @@ console.log('%cTask18', 'color: green; font-weight: bold;');
 //   }
 //   // Change code below this line
 
+// #endregion
+// #region task19
 console.log('%cTask19', 'color: green; font-weight: bold;');
 
 // Додай класу Admin метод constructor, який приймає один параметр - об'єкт налаштувань з двома властивостями email і accessLevel. Додай класу Admin публічну властивість accessLevel, значення якої буде передаватися під час виклику конструктора.
@@ -654,6 +736,8 @@ console.log('%cTask19', 'color: green; font-weight: bold;');
 //   console.log(mango.email); // "mango@mail.com"
 //   console.log(mango.accessLevel); // "superuser"
 
+// #endregion
+// #region task20
 console.log('%cTask20', 'color: green; font-weight: bold;');
 
 // Додай класу Admin наступні властивості і методи.
@@ -714,3 +798,5 @@ console.log('%cTask20', 'color: green; font-weight: bold;');
 //   console.log(mango.blacklistedEmails); // ["poly@mail.com"]
 //   console.log(mango.isBlacklisted("mango@mail.com")); // false
 //   console.log(mango.isBlacklisted("poly@mail.com")); // true
+
+// #endregion

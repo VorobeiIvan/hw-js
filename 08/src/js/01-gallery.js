83e06ebe8ef2e4df71d5ff9b8ecc1aaeda6a3404
VorobeiIvan <0675361905vi@gmail.com>
2024-02-26 19:22:14 +0200

    add initials files

---
+++ 08/src/js/01-gallery.js
@@ -0,0 +1,12 @@
+// Завдання 1 - бібліотека SimpleLightbox
+// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
+
+// Додай бібліотеку SimpleLightbox як залежність проекту, використовуючи npm (посилання на CDN з твоєї минулої роботи більше не потрібне).
+// Використовуй свій JavaScript код з попередньої домашньої роботи, але виконай рефакторинг з урахуванням того, що бібліотека була встановлена через npm (синтаксис import/export).
+// Для того щоб підключити CSS код бібліотеки в проект, необхідно додати ще один імпорт, крім того, що описаний в документації.
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
+
+console.log(galleryItems);
